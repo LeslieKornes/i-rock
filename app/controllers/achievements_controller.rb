3983dbc040902ff9e262b21db3fc4c7c965f7d37
Leslie Kornes <lkornes@covermymeds.com>
2018-05-12 22:50:27 -0400

    Add achievements_controller

---
+++ app/controllers/achievements_controller.rb
@@ -0,0 +1,8 @@
+class AchievementsController < ApplicationController
+
+  def new
+
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :achievement, only: [ :new, :create ]
+
   root to: 'welcome#index'
 
 end
