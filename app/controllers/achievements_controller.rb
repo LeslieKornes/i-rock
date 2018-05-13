09eaad244b54b8b88b9a22631b3536952af77d2d
Leslie Kornes <lkornes@covermymeds.com>
2018-05-12 23:35:21 -0400

    Got create achievement spec to pass :D

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -4,4 +4,16 @@ class AchievementsController < ApplicationController
     @achievement = Achievement.new
   end
 
+  def create
+    @achievement = Achievement.new(achievement_params)
+    if @achievement.save
+      redirect_to root_url, notice: 'Achievement has been created'
+    end
+  end
+
+  private
+  def achievement_params
+    params.require(:achievement).permit(:title, :description, :privacy, :cover_image, :featured)
+  end
+
 end
