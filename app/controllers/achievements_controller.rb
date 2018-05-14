039024482493221969b77935714b97038e668d11
Leslie Kornes <lkornes@covermymeds.com>
2018-05-13 23:12:27 -0400

    Create achievement_page_spec and get it green.

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -13,6 +13,10 @@ class AchievementsController < ApplicationController
     end
   end
 
+  def show
+    @achievement = Achievement.find(params[:id])
+  end
+
   private
   def achievement_params
     params.require(:achievement).permit(:title, :description, :privacy, :cover_image, :featured)
